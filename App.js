d21bc32fa3994e347b0e0bab2ec4800beb26b2bc
Faizan-Akram <100079631+faizan-blip@users.noreply.github.com>
2023-10-27 16:12:32 +0530

    a movie ticket website components using fetch api

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
-import Login from './Components/Login';
+
+import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import Moviecards from './Components/Moviecards';
+
 
 function App() {
   return (
-  <>
-  <Login/>
-  </>
+    <Router basename='/MOVIE' >
+    <>
+    <Switch>
+      <Route exact path='/'>
+   <Home/>
+    </Route>
+    <Route exact path="/next/:id">
+  <Moviecards/>
+    </Route>
+    </Switch>
+    </>
+    </Router>
   );
 }
 
